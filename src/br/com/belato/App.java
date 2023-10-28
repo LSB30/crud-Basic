87a50daf467d45dacba646461433c15166185e6d
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-10-28 14:45:31 -0300

    feat: 🛠 validating menu

--- src/br/com/belato/App.java
+++ src/br/com/belato/App.java
@@ -28,8 +28,10 @@ public class App {
 
     private static boolean isOpcaoValida(String opcao) {
         if("1".equals(opcao) || "2".equals(opcao) || "3".equals(opcao) || "4".equals(opcao)) {
-
+            return true;
         }
+
+        return false;
     }
 
     private static void sair() {
