9f39dbeeacbcbe507604b5676532e7b0a554c4de
Lucas Santana <122113277+llink7@users.noreply.github.com>
2023-10-28 14:43:26 -0300

    feat: 🛠 menu

---
+++ src/br/com/belato/App.java
@@ -0,0 +1,43 @@
+package br.com.belato;
+
+import br.com.belato.dao.ClienteMapDAO;
+import br.com.belato.dao.IClienteDAO;
+
+import javax.swing.*;
+
+public class App {
+
+    private static IClienteDAO iClienteDAO;
+
+    public static void main(String[] args) {
+        iClienteDAO = new ClienteMapDAO();
+        String opcao =
+                JOptionPane.showInputDialog(null, "Digite 1 para cadastro | 2 para consultar | 3 para exclusão | 4 para alteração | 5 para sair.", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+
+
+        while(!isOpcaoValida(opcao)) {
+            if("".equals(opcao)) {
+                sair();
+            }
+        }
+        if (isOpcaoCadastro(opcao)) {
+            JOptionPane.showInputDialog(null, "Digite 1 para cadastro | 2 para consultar | 3 para exclusão | 4 para alteração | 5 para sair.", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+
+        }
+    }
+
+    private static boolean isOpcaoValida(String opcao) {
+        if("1".equals(opcao) || "2".equals(opcao) || "3".equals(opcao) || "4".equals(opcao)) {
+
+        }
+    }
+
+    private static void sair() {
+        JOptionPane.showInputDialog(null, "Saindo...", "Sair", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+
+    }
+
+    private static boolean isOpcaoCadastro(String opcao) {
+    }
+}
--- src/br/com/belato/dao/ClienteMapDAO.java
+++ src/br/com/belato/dao/ClienteMapDAO.java
@@ -1,2 +1,56 @@
-package br.com.belato.dao;public class ClienteMapDAO {
+package br.com.belato.dao;
+
+import br.com.belato.domain.Cliente;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ClienteMapDAO implements IClienteDAO{
+
+    private Map<Long, Cliente> map;
+    public ClienteMapDAO() {
+        this.map = new HashMap<>();
+    }
+    @Override
+    public Boolean cadastrar(Cliente cliente) {
+        if(this.map.containsKey(cliente.getCpf())) {
+            return false;
+        }
+        this.map.put(cliente.getCpf(), cliente);
+        return true;
+    }
+
+    @Override
+    public void excluir(Long cpf) {
+        Cliente clienteCadastrado = this.map.get(cpf);
+        if(clienteCadastrado != null) {
+            this.map.remove(clienteCadastrado.getCpf());
+        }
+    }
+
+    @Override
+    public void alterar(Cliente cliente) {
+        Cliente clienteCadastrado = this.map.get(cliente.getCpf());
+        if(clienteCadastrado != null) {
+            clienteCadastrado.setCidade(cliente.getCidade());
+            clienteCadastrado.setNome(cliente.getNome());
+            clienteCadastrado.setEnd(cliente.getEnd());
+            clienteCadastrado.setEstado(cliente.getEstado());
+            clienteCadastrado.setNumero(cliente.getNumero());
+            clienteCadastrado.setTel(cliente.getTel());
+        }
+    }
+
+    @Override
+    public Cliente consultar(Long cpf) {
+        return this.map.get(cpf);
+    }
+
+    @Override
+    public Collection<Cliente> buscarTodos() {
+        return this.map.values();
+    }
+
 }
